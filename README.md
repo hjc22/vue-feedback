ee9221e69793e1fee39c98e68ddf04f7e9be625d
hjc22 <549008556@qq.com>
2017-03-09 11:50:05 +0800

    first

---
+++ README.md
@@ -0,0 +1,25 @@
+# vue-feedback
+
+> Vue v2.0 自定义指令
+
+顾名思义，这个是为vue移动端项目打造的触摸反馈指令，比如点击一个按钮会有样式反馈;默认是透明度的变化反馈，当然你也可以自定义样式；
+
+## 安装步骤
+
+``` bash
+# 安装 npm 模块
+npm install vue-feedback --save
+
+#项目引入
+import vueFeedback from 'vue-feedback'
+
+# vue.use()加载
+Vue.use(vueFeedback)
+
+# 组件内使用
+<button v-fb>按钮</button>
+
+# 高级用法(自定义样式，添加写好的class名)
+<button v-fb="{cls:'buttonActive'}"
+
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.install = factory());
+}(this, (function () { 'use strict';
+
+// var inBrowser = typeof window !== 'undefined';
+
+
+
+var vueTouchFeedback =function (Vue) {
+
+        var touchFeedback={
+            bind(el,binding){
+               el.addEventListener('touchstart',handleStart.bind(null,el,binding.value),false)
+               el.addEventListener('touchend',handleEnd.bind(null,el,binding.value),false)
+            },
+            unbind(el){
+               el.removeEventListener('touchstart',handleStart,false)
+               el.removeEventListener('touchend',handleEnd,false)
+               setStyle(el,{
+                 webkitTransition:'none',
+                 transition:'none'
+               })
+            }
+        }
+
+        Vue.directive('fb',touchFeedback);
+
+};
+
+return vueTouchFeedback;
+
+function handleStart(el,cls){
+    if(cls) addClass(el,cls.cls);
+    else setStyle(el,{
+            webkitTransition:'opacity 0.2s',
+            transition:'opacity 0.2s',
+            opacity:'0.2'
+         });
+}
+function handleEnd(el,cls){
+    if(cls) removeClass(el,cls.cls);
+    else setStyle(el,{
+           opacity:'1'
+         });
+}
+
+function setStyle(el,data){
+    for(var i in data){
+        el.style[i]=data[i];
+    }
+}
+
+function hasClass(el, cls) {
+  if (!el || !cls) return false;
+  if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.');
+  if (el.classList) {
+    return el.classList.contains(cls);
+  } else {
+    return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+};
+
+/* istanbul ignore next */
+function addClass(el, cls) {
+  if (!el) return;
+  var curClass = el.className;
+  var classes = (cls || '').split(' ');
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i];
+    if (!clsName) continue;
+
+    if (el.classList) {
+      el.classList.add(clsName);
+    } else {
+      if (!hasClass(el, clsName)) {
+        curClass += ' ' + clsName;
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass;
+  }
+};
+
+/* istanbul ignore next */
+function removeClass(el, cls) {
+  if (!el || !cls) return;
+  var classes = cls.split(' ');
+  var curClass = ' ' + el.className + ' ';
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i];
+    if (!clsName) continue;
+
+    if (el.classList) {
+      el.classList.remove(clsName);
+    } else {
+      if (hasClass(el, clsName)) {
+        curClass = curClass.replace(' ' + clsName + ' ', ' ');
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass.trim();
+  }
+};
+
+
+console.log(vueTouchFeedback)
+
+console.log(module)
+
+
+})));
